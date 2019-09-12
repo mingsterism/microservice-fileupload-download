fb2e934b01b7f0ff6aef1f22243db479b4a1fa1f
Justin <justinthong93@gmail.com>
2019-09-12 23:11:34 +0800

    try pushhing

--- test/main.js
+++ test/main.js
@@ -26,8 +26,8 @@ describe('Array', function() {
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(4));
     });
-    it('try error', function(){
-      assert.equal(1,2);
-    }) 
+    //it('try error', function(){
+    //  assert.equal(1,2);
+    //}) 
   });
 });
